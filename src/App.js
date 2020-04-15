7fe1a08ecc0abeba914377ed512f83cd6261cfa5
Ashley Ahn <ashleyjsahn@gmail.com>
2020-04-15 15:14:15 -0700

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAhmO59ZWHYd9Du8OvmcSRbgoam4rDBj9g",
+  authDomain: "shopping-cart-b046b.firebaseapp.com",
+  databaseURL: "https://shopping-cart-b046b.firebaseio.com",
+  projectId: "shopping-cart-b046b",
+  storageBucket: "shopping-cart-b046b.appspot.com",
+  messagingSenderId: "352224833111",
+  appId: "1:352224833111:web:b89c19fb7de465cc41bd12",
+  measurementId: "G-8QMNBVB5JC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 function App() {
   return (
